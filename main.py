6e0eeb33559b8e35de552fd3defa313a63a03a57
Tanupong <>
2024-08-16 10:38:33 +0000

    chg: test remove main

--- main.py
+++ main.py
@@ -32,6 +32,3 @@ async def query_data(request: Request):
     "id":id,
     "password":password}
     return result
-
-if __name__ == "__main__":
-    uvicorn.run("main:app",host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
