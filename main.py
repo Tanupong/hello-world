dfdb43d1cd65d3ee961d6624ca37708faed39529
Tanupong <>
2024-08-16 10:45:31 +0000

    main again

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -10,4 +10,4 @@ async def hello_world():
     return {"message": f"Hello! This is version {app_version} of my application."}
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
